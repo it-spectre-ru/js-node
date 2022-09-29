a00f066c9fa2b4d02385c3c18ba199d67d1e5ecc
s <1>
2022-09-29 19:06:54 +0500

    own realization commonjs modules with comment

--- 02-modules/f-commonjs.js
+++ 02-modules/f-commonjs.js
@@ -9,11 +9,18 @@ const pseudoRequire = (name) => {
   console.log(`Intercepted require: ${name}`);
 };
 
+// заменитель require. Асинхронная функция. На вход принимает имя файла и контекст, который будет считаться глобальным внутри этого файла
 const load = async (filePath, sandbox) => {
+  //считываю содержимое этого файла. говорим что оно в utf кодировке
+  //жду пока промис все это вернет
   const src = await fs.readFile(filePath, 'utf8');
+  // оборачиваю исходник считанный из файла в конструкцию. стрелочную функцию
   const code = `(require, module, __filename, __dirname) => {\n${src}\n}`;
+  // получил экземпляр скрипта
   const script = new vm.Script(code);
+  // создаю контекст для исполнения скрипта. перепаковал , зафризил контекст sandbox, получил новый глобал
   const context = vm.createContext(Object.freeze({ ...sandbox }));
+  // исполнил скрипт и получил обертку. ссылка на функцию , которую задали в виде строки
   const wrapper = script.runInContext(context, RUN_OPTIONS);
   const module = {};
   wrapper(pseudoRequire, module, filePath, __dirname);
