6b8cd559d18339285aeed7140645eaa672c93ec7
s <1>
2022-10-25 10:45:55 +0500

    Блок заданий №3
    заменил самодельную систему модульности для написания API на commonjs

--- 03-architecture/c-commonjs/api/country.js
+++ 03-architecture/c-commonjs/api/country.js
@@ -1,6 +1,9 @@
-const country = db('country');
+'use strict';
 
-({
+const db = require('../db.js');
+const country = db.crud('country');
+
+module.exports = {
   async read(id) {
     console.log({ db });
     return country.read(id);
@@ -10,4 +13,4 @@ const country = db('country');
     const sql = 'SELECT * from country where name like $1';
     return country.query(sql, [mask]);
   },
-});
+};
